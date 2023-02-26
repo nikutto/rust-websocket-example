fbcf4385e6abaf9e76fbba8bedad1b69b3eb66ad
Jumpei Mori <nikutto.kom@gmail.com>
2023-02-26 16:14:40 +0900

    Implement websocket echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{net::TcpListener, thread::spawn};
+
+use tungstenite::accept;
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("127.0.0.2:9001").unwrap();
+    for stream in server.incoming() {
+        spawn (move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let msg = websocket.read_message();
+                match msg {
+                    Ok(msg) => {
+                        if msg.is_binary() || msg.is_text() {
+                            websocket.write_message(msg).unwrap();
+                        }
+                    }
+                    Err(err) => {
+                        println!("{:?}", err);
+                        break;
+                    }
+                }
+            }
+        });
+    }
 }
